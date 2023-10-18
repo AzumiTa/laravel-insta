741572b7222815b698d89adc42ba64c17b7ced6a
AzumiTa <v.azumimimi.v@gmail.com>
2023-10-18 13:37:53 +0900

    first commit

---
+++ app/Http/Controllers/FollowController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Follow;
+use Illuminate\Support\Facades\Auth;
+
+class FollowController extends Controller
+{
+    private $follow;
+
+    public function __construct(Follow $follow){
+        $this->follow = $follow;
+    }
+
+    public function store($user_id)
+    {
+        $this->follow->follower_id = Auth::user()->id;
+        $this->follow->following_id = $user_id;
+        $this->follow->save();
+
+        return redirect()->back();
+
+    }
+
+    public function destroy($user_id)
+    {
+        $this->follow
+        ->where('follower_id', Auth::user()->id)
+        ->where('following_id', $user_id)
+        ->delete();
+
+        return redirect()->back();
+
+             //put your where condition
+             //example: ->where('follower_id, Auth::user()->id)
+             //continue the code and use delete()
+             //Create new route and use the route
+    }
+}
+
+//= assign | == compare | === comparing with datatype
---
+++ app/Models/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    #A post belongs to a user
+    #To get the owner of the post
+    public function user()
+    {
+        return $this->belongsTo(user::class)->withTrashed();
+    }
+
+    #To get the categories under a post
+    public function categoryPost()
+    {
+        return $this->hasMany(categoryPost::class);
+    }
+    #To get all the comment of a post
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    #Ti get the likes of a post
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    #Return TRUE if the Auth user already likes the post
+    public function isliked()
+    {
+        return $this->likes()->where('user_id', Auth::user()->id)->exists();
+    }
+}
